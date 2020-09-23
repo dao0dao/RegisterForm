969f6870b55b82a5fecaab27d6ce9201f08e28ca
Demid Greshnikov <d_greshnikov@tlen.pl>
2020-09-23 11:34:23 +0200

    Updates

---
+++ precache-manifest.3cd8a797db1825f4252f0b592f5c54ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37178253958dd3f715a2953c2a6f54f2",
+    "url": "/RegisterForm/index.html"
+  },
+  {
+    "revision": "66a2e65650197f16de43",
+    "url": "/RegisterForm/static/css/main.dfef2287.chunk.css"
+  },
+  {
+    "revision": "d66f6875e234b03824bf",
+    "url": "/RegisterForm/static/js/2.dd0c7d08.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RegisterForm/static/js/2.dd0c7d08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66a2e65650197f16de43",
+    "url": "/RegisterForm/static/js/main.a4da8eb8.chunk.js"
+  },
+  {
+    "revision": "365e7aa70bab1a094807",
+    "url": "/RegisterForm/static/js/runtime-main.6d5517f6.js"
+  }
+]);
